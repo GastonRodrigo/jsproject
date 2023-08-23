e476fb59297490670e9a4f582817d1cd1ab29f7e
GastonRodrigo <cordoba.gastonr@yahoo.com>
2023-08-22 21:34:37 -0300

    agregados dom json events storage

--- index.js
+++ index.js
@@ -4,12 +4,9 @@
 //BUSCADOR DE PRODUCTOS
 //CARRITO DE COMPRA Y PEDIDO (SE ENVIA A WHATSAPP)
 
-alert('Bienvenido a Los Sauces Dietetica')
-
-
 // CONSTRUCTOR DE PRODUCTO
 class Producto {
-    constructor (codigo, nombre, cantidad, precio) {
+    constructor(codigo, nombre, cantidad, precio) {
         this.codigo = codigo;
         this.nombre = nombre;
         this.cantidad = cantidad;
@@ -18,40 +15,89 @@ class Producto {
 }
 
 // FUNCION PARA CREAR PRODUCTOS
-
-function crearProducto () {
-    const codigo = parseInt (prompt ('Ingrese el codigo del Producto nuevo:'));
-    const nombre = prompt ('Ingrese el nombre del Producto nuevo:');
-    const cantidad = parseInt (prompt ('Ingrese la cantidad de Kilos o Unidades (en numero):'));
-    const precio = parseFloat (prompt ('Ingrese el precio del Producto nuevo:'));
+function crearProducto() {
+    const codigo = parseInt(prompt('Ingrese el codigo del Producto nuevo:'));
+    const nombre = prompt('Ingrese el nombre del Producto nuevo:');
+    const cantidad = parseInt(prompt('Ingrese la cantidad de Kilos o Unidades (en numero):'));
+    const precio = parseFloat(prompt('Ingrese el precio del Producto nuevo:'));
 
     const productoNuevo = new Producto(codigo, nombre, cantidad, precio);
     return productoNuevo;
-
 }
 
-const listaProductos = [];
+const productosPredeterminados = [
+    new Producto(1, 'Hummus Felices las vacas', 10, 1500),
+    new Producto(2, 'Karnevil Felices las vacas', 15, 2450),
+    new Producto(3, 'VDRINK Almendras', 8, 1270),
+    new Producto(4, 'Alfajor de Almendras', 12, 875)
+];
+
+const listaProductos = [...productosPredeterminados];
 
 let agregarProducto = true;
 
 while (agregarProducto) {
-    const productoNuevo = crearProducto ();
-    listaProductos.push (productoNuevo);
+    const productoNuevo = crearProducto();
+    listaProductos.push(productoNuevo);
 
-    const seguirCreando = prompt ('Agregar otro producto?');
-    agregarProducto = seguirCreando == 'si';
+    const seguirCreando = prompt('Agregar otro producto?');
+    agregarProducto = seguirCreando === 'si';
 }
 
-console.log ('Lista de productos:');
-console.log (listaProductos);
+console.log('Lista de productos:');
+console.log(listaProductos);
 
-// PROMPT PARA BUSCAR PRODUCTOS
+// BOTON AGREGAR NUEVO PRODUCTO
+const crearProductoBtn = document.getElementById('crear-producto-btn');
 
-const buscarProducto = prompt('Ingrese el nombre del producto que desea buscar:');
-const productoEncontrado = listaProductos.find(producto => producto.nombre === buscarProducto);
+crearProductoBtn.addEventListener('click', () => {
+    const productoNuevo = crearProducto();
+    listaProductosGuardados.push(productoNuevo);
 
-if (productoEncontrado) {
-    console.log(`El precio del producto "${productoEncontrado.nombre}" es $${productoEncontrado.precio}`);
-} else {
-    console.log(`No hay stock del producto consultado`);
+    const listaProductosGuardadosJSON = JSON.stringify(listaProductosGuardados);
+    localStorage.setItem('productos', listaProductosGuardadosJSON);
+
+    
+    mostrarProductosEnDOM();
+});
+
+// GUARDAR EN STORAGE
+const listaProductosJSON = JSON.stringify(listaProductos);
+localStorage.setItem('productos', listaProductosJSON);
+
+// CARGAR DESDE STORAGE
+const listaProductosGuardadosJSON = localStorage.getItem('productos');
+const listaProductosGuardados = JSON.parse(listaProductosGuardadosJSON) || [];
+
+// MODIFICAR EL DOM Y DETECTAR EVENTOS DE USUARIO
+const carritoLista = document.getElementById('carrito-lista');
+const productosContainer = document.getElementById('productos-container');
+
+function mostrarProductosEnDOM() {
+    productosContainer.innerHTML = '';
+
+    listaProductosGuardados.forEach(producto => {
+        const productoDiv = document.createElement('div');
+        productoDiv.innerHTML = `
+            <div class="producto-info">
+                <p><strong>Nombre:</strong> ${producto.nombre}</p>
+                <p><strong>Precio:</strong> $${producto.precio.toFixed(2)}</p>
+                <button class="agregar-carrito" data-codigo="${producto.codigo}">Agregar al carrito</button>
+            </div>
+        `;
+        productosContainer.appendChild(productoDiv);
+    });
 }
+
+mostrarProductosEnDOM();
+
+document.addEventListener('click', event => {
+    if (event.target.classList.contains('agregar-carrito')) {
+        const codigoProducto = parseInt(event.target.getAttribute('data-codigo'));
+        const productoSeleccionado = listaProductosGuardados.find(producto => producto.codigo === codigoProducto);
+
+        if (productoSeleccionado) {
+            carritoLista.innerHTML += `<li>${productoSeleccionado.nombre} - $${productoSeleccionado.precio.toFixed(2)}</li>`;
+        }
+    }
+});
